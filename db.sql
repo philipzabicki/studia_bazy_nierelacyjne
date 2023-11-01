9dc96a3210b5a7662de955951c1cc58a5fb7271b
philipz <philipz.abicki@gmail.com>
2023-11-01 18:34:48 +0100

    init

---
+++ db.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS document;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS document_tag;
+
+CREATE TABLE document (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name CHAR(64) NOT NULL,
+    description TEXT
+    );
+
+CREATE TABLE tag (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name CHAR(64) NOT NULL,
+    description TEXT
+    );
+
+CREATE TABLE document_tag (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    document_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(document_id) REFERENCES document(id),
+    FOREIGN KEY(tag_id) REFERENCES tag(id)
+    );
